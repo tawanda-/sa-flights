2f86c7b78fab4689c22775140f3ad010a7b85dc2
Tawanda <tmuhwati@gmail.com>
2022-04-08 04:16:31 +0200

    ammend airport schema

--- backend/saflightsapi/schema.py
+++ backend/saflightsapi/schema.py
@@ -1,3 +1,5 @@
+from re import A
+from typing_extensions import Required
 import graphene
 from graphene_django import DjangoObjectType
 from .models import Airport
@@ -7,10 +9,26 @@ class AirportType(DjangoObjectType):
         model = Airport
         fields = (
             'iata_code',
-            'airport_name',
+            'name',
             'municipality',
             'latitude_deg',
             'longitude_deg',
             'elevation',
             'image_url'
         )
+
+class Query(graphene.ObjectType):
+
+    airports = graphene.List(AirportType)
+    airport = graphene.Field(AirportType, iata_code=graphene.String(required=True))
+        
+    def resolve_airports(root, info):
+        return Airport.objects.all()
+
+    def resolve_airport(root, info, iata_code):
+        try:
+            return Airport.objects.get(iata_code=iata_code)
+        except Airport.DoesNotExist:
+            return None
+    
+schema = graphene.Schema(query=Query)
