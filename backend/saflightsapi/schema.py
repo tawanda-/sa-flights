4faa4ebb1dfde8e98ebbf1f3b19636abb5ecac34
Tawanda <tmuhwati@gmail.com>
2022-04-08 17:22:03 +0200

    added flights to schema

--- backend/saflightsapi/schema.py
+++ backend/saflightsapi/schema.py
@@ -1,6 +1,7 @@
+from ast import Num
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Airport
+from .models import Airport, Flight, FlightDetail
 
 class AirportType(DjangoObjectType):
     class Meta:
@@ -12,13 +13,48 @@ class AirportType(DjangoObjectType):
             'latitude_deg',
             'longitude_deg',
             'elevation',
-            'image_url'
+            'image_url',
+        )
+
+class FlightType(DjangoObjectType):
+    class Meta:
+        model = Flight
+        fields = (
+            'number',
+            'date',
+            'status',
+            'airline',
+        )
+
+class FlightDetailType(DjangoObjectType):
+    class Meta:
+        model = FlightDetail
+        fields = (
+            'number',
+            'status',
+            'airport',
+            'iata',
+            'icao',
+            'terminal',
+            'gate',
+            'baggage',
+            'time_delay',
+            'time_scheduled',
+            'time_estimated',
+            'time_actual',
+            'estimated_runway',
+            'actual_runway',
         )
 
 class Query(graphene.ObjectType):
 
     airports = graphene.List(AirportType)
     airport = graphene.Field(AirportType, iata_code=graphene.String(required=True))
+
+    flights = graphene.List(FlightType)
+    flight = graphene.Field(FlightType, number=graphene.String(required=True))
+
+    flightDetails = graphene.Field(FlightDetailType, number=graphene.String(required=True))
         
     def resolve_airports(root, info):
         return Airport.objects.all()
@@ -27,6 +63,21 @@ class Query(graphene.ObjectType):
         try:
             return Airport.objects.get(iata_code=iata_code)
         except Airport.DoesNotExist:
+            return None
+
+    def resolve_flights(root, info):
+        return Flight.objects.all()
+
+    def resolve_flight(root, info, number):
+        try:
+            return Flight.objects.get(number=number)
+        except Flight.DoesNotExist:
+            return None
+
+    def resolve_flightDetail(root, info, number):
+        try:
+            return FlightDetailType.objects.get(number=number)
+        except FlightDetail.DoesNotExist:
             return None
     
 schema = graphene.Schema(query=Query)
