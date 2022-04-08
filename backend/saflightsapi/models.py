d0f13ec053277fbce85565c243f6e2f97945cbff
Tawanda <tmuhwati@gmail.com>
2022-04-08 03:18:13 +0200

    added flight model

--- backend/saflightsapi/models.py
+++ backend/saflightsapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Flight(models.Model):
+    number = models.CharField(max_length=20)
+    date = models.DateField(null=False, blank=False)
+    status = models.CharField(max_length=20, blank=True)
+    airline = models.CharField(max_length=250, blank=True)
+    
+    def __str__(self):
+        return self.number
