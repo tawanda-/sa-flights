9f6486a6829c89d584f8df6474f4b7c7832fc6b5
Tawanda <tmuhwati@gmail.com>
2022-04-08 02:49:01 +0200

    add graphene-django for handling graphQL queries

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'saflightsapi'
+    'saflightsapi',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
