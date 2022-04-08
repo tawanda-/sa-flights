0e9f33bdafb16fd097696faadf83beeeecc3931c
Tawanda <tmuhwati@gmail.com>
2022-04-08 03:28:58 +0200

    added flight details model

--- backend/saflightsapi/models.py
+++ backend/saflightsapi/models.py
@@ -8,3 +8,31 @@ class Flight(models.Model):
     
     def __str__(self):
         return self.number
+
+class FlightDetail(models.Model):
+
+    STATUS_CHOICE = [
+        ('DEPARTURE', 'departure'),
+        ('ARRIVAL', 'arrival'),
+    ]
+
+    number = models.CharField(max_length=20)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICE
+    )
+    airport = models.CharField(max_length=250)
+    iata = models.CharField(max_length=10)
+    icao = models.CharField(max_length=10)
+    terminal = models.CharField(max_length=250)
+    gate = models.CharField(max_length=250)
+    baggage = models.CharField(max_length=250)
+    time_delay = models.TimeField(blank=True, null=True)
+    time_scheduled = models.TimeField(blank=True, null=True)
+    time_estimated = models.TimeField(blank=True, null=True)
+    time_actual = models.TimeField(blank=True, null=True)
+    estimated_runway = models.CharField(max_length=250, blank=True, null=True)
+    actual_runway = models.CharField(max_length=250, blank=True, null=True)
+    
+    def __str__(self):
+        return self.flight_number
