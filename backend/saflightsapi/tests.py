3cbd9f7d0dfe5cee82f54f5b113152117e5e935b
Tawanda <tmuhwati@gmail.com>
2022-04-08 06:42:30 +0200

    Airport test cases

--- backend/saflightsapi/tests.py
+++ backend/saflightsapi/tests.py
@@ -1,3 +1,50 @@
+import json
 from django.test import TestCase
+from .models import Airport
+from graphene_django.utils.testing import GraphQLTestCase
 
-# Create your tests here.
+class SAFlightsApiTestCase(GraphQLTestCase):
+
+    def setUp(self):
+        Airport.objects.create(
+            iata_code ='MQP', 
+            name ='Kruger Mpumalanga International Airport', 
+            municipality ='Mpumalanga',
+            latitude_deg = '-25.38319969',
+            longitude_deg =	'31.10560036',
+            elevation = '2829',
+            image_url = "https://imgbox.com/rC5HhVTD"
+        )
+        
+
+    def test_airports_query(self):
+        response = self.query(
+            '''
+            query {
+                airports{
+                    name,municipality,iataCode,latitudeDeg,longitudeDeg,elevation,imageUrl
+                }
+            }
+            ''',
+            op_name = 'airports'
+        )
+
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+
+    def test_airport_query(self):
+        response = self.query(
+            '''
+            query {
+                airport{
+                    name,municipality,iataCode,latitudeDeg,longitudeDeg,elevation,imageUrl
+                }
+            }
+            ''',
+            op_name='airport',
+            variables={'iataCode': 'MQP'}
+        )
+
+        content = json.loads(response.content)
+
+        self.assertResponseNoErrors(response)
