024089cf5a6d4a98eb21cee59189107825e388ec
Tawanda <tmuhwati@gmail.com>
2022-04-20 15:23:50 +0200

    Add new models to admin panel

--- backend/saflightsapi/admin.py
+++ backend/saflightsapi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Airport, Flight, FlightDetail
+from .models import Airport, Arrival, Departure, Flight
 
 admin.site.register(Airport)
 admin.site.register(Flight)
-admin.site.register(FlightDetail)
+admin.site.register(Departure)
+admin.site.register(Arrival)
