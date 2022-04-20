2b38fbe6f50ebbe91753c0ea47a9c8c255f0a5a4
Tawanda <tmuhwati@gmail.com>
2022-04-20 15:23:04 +0200

    Added new models:  Aircraft, Departure and Arrival. Removed FlightDetail model

--- backend/saflightsapi/models.py
+++ backend/saflightsapi/models.py
@@ -1,29 +1,40 @@
+from pyexpat import model
+from unicodedata import category
 from django.db import models
 
-# airport details
 class Airport(models.Model):
-    icao_code = models.CharField(max_length=25, primary_key = True)
-    iata_code = models.CharField(max_length=25)
-    name = models.CharField(max_length=250)
-    municipality = models.CharField(max_length=250)
+
+    class Meta:
+       ordering = ['name']
+
+    icao_code = models.CharField(max_length=25, blank=True, null=True)
+    iata_code = models.CharField(max_length=25, blank=True, null=True)
+    name = models.CharField(max_length=250, blank=True, null=True)
+    city = models.CharField(max_length=250, blank=True, null=True)
+    country = models.CharField(max_length=250, blank=True, null=True)
     latitude_deg = models.FloatField(blank=True, null=True)
     longitude_deg = models.FloatField(blank=True, null=True)
     elevation = models.IntegerField(blank=True, null=True)
     image_url = models.URLField(blank=True, null=True)
     
     def __str__(self):
-        return self.name 
+        return '%s %s' % (self.name, self.icao_code)
 
-class FlightDetail(models.Model):
+class Aircraft(models.Model):
+
+    category = models.CharField(max_length=100, blank=True, null=True)
+    type = models.CharField(max_length=100, blank=True, null=True)
+    icao = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.type
+
+class Departure(models.Model):
 
     class Meta:
        ordering = ['time_scheduled']
 
-    STATUS_CHOICE = [
-        ('DEPARTURE', 'departure'),
-        ('ARRIVAL', 'arrival'),
-    ]
-    status = models.CharField(max_length=10,choices=STATUS_CHOICE)
+    airport = models.ForeignKey('Airport', on_delete=models.CASCADE)
     terminal = models.CharField(max_length=250, blank=True, null=True)
     gate = models.CharField(max_length=250, blank=True, null=True)
     baggage = models.CharField(max_length=250, blank=True, null=True)
@@ -35,20 +46,37 @@ class FlightDetail(models.Model):
     actual_runway = models.CharField(max_length=250, blank=True, null=True)
 
     def __str__(self):
-        return '%s %s' % (self.time_scheduled, self.status)
+        return '%s %s' % (self.time_scheduled, self.airport)
+
+class Arrival(models.Model):
+
+    class Meta:
+       ordering = ['time_scheduled']
+
+    airport = models.ForeignKey('Airport', on_delete=models.CASCADE)
+    terminal = models.CharField(max_length=250, blank=True, null=True)
+    gate = models.CharField(max_length=250, blank=True, null=True)
+    baggage = models.CharField(max_length=250, blank=True, null=True)
+    time_delay = models.IntegerField(blank=True, null=True)
+    time_scheduled = models.TimeField(blank=True, null=True)
+    time_estimated = models.TimeField(blank=True, null=True)
+    time_actual = models.TimeField(blank=True, null=True)
+    estimated_runway = models.CharField(max_length=250, blank=True, null=True)
+    actual_runway = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.time_scheduled, self.airport)
 
 class Flight(models.Model):
 
-    number = models.CharField(max_length=20, blank=True, null=True)
+    number = models.CharField(max_length=20, blank=True, null=True, unique=True)
     iata_code = models.CharField(max_length=20, blank=True, null=True)
     icao_code = models.CharField(max_length=20, blank=True, null=True)
-    date = models.DateField(null=False, blank=False) #icao/iata code
-    source_airport = models.ForeignKey('Airport', related_name='departure_airport_set',on_delete=models.CASCADE)
-    destination_airport = models.ForeignKey('Airport', related_name='arrival_airport_set',on_delete=models.CASCADE)
+    date = models.DateField(null=False, blank=False) 
     status = models.CharField(max_length=20, blank=True, null=True)
     airline = models.CharField(max_length=250, blank=True, null=True)
-    departure = models.ForeignKey('FlightDetail', related_name='departure_flight_set',on_delete=models.CASCADE)
-    arrival = models.ForeignKey('FlightDetail', related_name='arrival_flight_set', on_delete=models.CASCADE)
-    
+    departure = models.ForeignKey('Departure', on_delete=models.CASCADE)
+    arrival = models.ForeignKey('Arrival', on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.airline
+        return '%s %s %s %s' % (self.number, self.airline, self.departure, self.arrival)
