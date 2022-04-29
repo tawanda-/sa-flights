432985e89e9dfef567883c91341477b976a6542f
Tawanda <tmuhwati@gmail.com>
2022-04-29 10:20:42 +0200

    added no flights message

--- frontend/src/components/Flights.js
+++ frontend/src/components/Flights.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Flight from "./Flight";
+import NoFlights from "../static/svg/x_airplane.png"
 
 function Flights(props) {
+  console.log(Object.keys(props.flights).length);
   return (
     <div className="container">
-      {Object.values( props.flights ).map((flight, index) => (
+      {Object.keys(props.flights).length > 0 ? (
+        Object.values(props.flights).map((flight, index) => (
           <Flight flight={flight} index={flight.number} key={index} />
-      ))}
+        ))
+      ) : (
+        <div class="alert alert-light text-center" role="alert">
+          <img src={NoFlights} style={{width:"64px"}}/>
+           &nbsp;&nbsp;&nbsp;No flights available.
+        </div>
+      )}
     </div>
   );
 }
