4e834a3b2e6049089ba205e7171f611cc61759fc
Tawanda <tmuhwati@gmail.com>
2022-04-29 10:17:20 +0200

    refactored flight model

--- backend/saflightsapi/models.py
+++ backend/saflightsapi/models.py
@@ -28,66 +28,36 @@ class Aircraft(models.Model):
     def __str__(self) -> str:
         return self.type
 
-class Departure(models.Model):
-
-    class Meta:
-       ordering = ['time_scheduled']
-
-    airport = models.ForeignKey('Airport', on_delete=models.CASCADE)
-    terminal = models.CharField(max_length=250, blank=True, null=True)
-    gate = models.CharField(max_length=250, blank=True, null=True)
-    baggage = models.CharField(max_length=250, blank=True, null=True)
-    time_delay = models.IntegerField(blank=True, null=True)
-    time_scheduled = models.TimeField(blank=True, null=True)
-    time_estimated = models.TimeField(blank=True, null=True)
-    time_actual = models.TimeField(blank=True, null=True)
-    estimated_runway = models.CharField(max_length=250, blank=True, null=True)
-    actual_runway = models.CharField(max_length=250, blank=True, null=True)
-
-    def __str__(self):
-        return '%s %s' % (self.time_scheduled, self.airport)
-
-class Arrival(models.Model):
-
-    class Meta:
-       ordering = ['time_scheduled']
-
-    airport = models.ForeignKey('Airport', on_delete=models.CASCADE)
-    terminal = models.CharField(max_length=250, blank=True, null=True)
-    gate = models.CharField(max_length=250, blank=True, null=True)
-    baggage = models.CharField(max_length=250, blank=True, null=True)
-    time_delay = models.IntegerField(blank=True, null=True)
-    time_scheduled = models.TimeField(blank=True, null=True)
-    time_estimated = models.TimeField(blank=True, null=True)
-    time_actual = models.TimeField(blank=True, null=True)
-    estimated_runway = models.CharField(max_length=250, blank=True, null=True)
-    actual_runway = models.CharField(max_length=250, blank=True, null=True)
-
-    def __str__(self):
-        return '%s %s' % (self.time_scheduled, self.airport)
-
 class Flight(models.Model):
 
-    number = models.CharField(max_length=20, blank=True, null=True, unique=True)
+    number = models.CharField(max_length=20, blank=True, null=True)
     iata_code = models.CharField(max_length=20, blank=True, null=True)
     icao_code = models.CharField(max_length=20, blank=True, null=True)
     date = models.DateField(null=False, blank=False) 
     status = models.CharField(max_length=20, blank=True, null=True)
     airline = models.CharField(max_length=250, blank=True, null=True)
-    departure = models.ForeignKey('Departure', on_delete=models.CASCADE)
-    arrival = models.ForeignKey('Arrival', on_delete=models.CASCADE)
+
+    departure_airport = models.ForeignKey('Airport', related_name='departurex',on_delete=models.CASCADE)
+    departure_terminal = models.CharField(max_length=250, blank=True, null=True)
+    departure_gate = models.CharField(max_length=250, blank=True, null=True)
+    departure_baggage = models.CharField(max_length=250, blank=True, null=True)
+    departure_time_delay = models.IntegerField(blank=True, null=True)
+    departure_time_scheduled = models.TimeField(blank=True, null=True)
+    departure_time_actual = models.TimeField(blank=True, null=True)
+
+    arrival_airport = models.ForeignKey('Airport', related_name='arrivalx',on_delete=models.CASCADE)
+    arrival_terminal = models.CharField(max_length=250, blank=True, null=True)
+    arrival_gate = models.CharField(max_length=250, blank=True, null=True)
+    arrival_baggage = models.CharField(max_length=250, blank=True, null=True)
+    arrival_time_delay = models.IntegerField(blank=True, null=True)
+    arrival_time_scheduled = models.TimeField(blank=True, null=True)
+    arrival_time_actual = models.TimeField(blank=True, null=True)
 
     def __str__(self):
-        return '%s %s %s %s' % (self.number, self.airline, self.departure, self.arrival)
+        return f"{self.number}, {self.airline}"
 
 class AirportAdmin(admin.ModelAdmin):
     search_fields = ["icao_code", "iata_code", "city", "country", "name"]
 
 class FlightAdmin(admin.ModelAdmin):
     search_fields = ["icao_code", "iata_code", "number", "airline", "departure__time_scheduled"]
-
-class ArrivalAdmin(admin.ModelAdmin):
-    search_fields = ["airport__name", "airport__icao_code", "airport__iata_code"]
-
-class DepartureAdmin(admin.ModelAdmin):
-    search_fields = ["airport__name", "airport__icao_code", "airport__iata_code"]
